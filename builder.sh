185f09c1a923116bdfa111a7385f18b94c87bbe2
Or Yaacov <oryaacov@live.com>
2025-05-12 19:35:35 +0300

    feat(builder): build both source package and wheel

--- builder.sh
+++ builder.sh
@@ -19,6 +19,22 @@ validate_params() {
     fi
 }
 
+validate_python() {
+  if ! command -v python >/dev/null 2>&1; then
+    echo "Error: python executable not found" >&2
+    exit 1
+  fi
+
+  if ! python -c "import build" >/dev/null 2>&1; then
+    echo "Error: python does not have the 'build' module installed" >&2
+    exit 1
+  fi
+}
+
+print_python_version() {
+    python -V
+}
+
 prepare_directories() {
     local WORKING_DIR="$1"
     local TEMP_DIR="${WORKING_DIR}/temp"
@@ -34,20 +50,25 @@ clone_repository() {
     git fetch --tags
 }
 
+tag_to_version() {
+    local tag="$1"
+    echo "${TAG}" | sed 's/^v//'
+}
+
 build_package() {
     local TEMP_DIR="$1"
     local DIST_DIR="$2"
     local TAG="$3"
-    local VERSION="$(echo "${TAG}" | sed 's/^v//')"
+    local VERSION="$(tag_to_version "${TAG}")"
     local PACKAGE_DIR="${TEMP_DIR}/package-${VERSION}"
     local PACKAGE_NAME="nftables-${VERSION}.tar.gz"
     
-    echo "building nftables ${TAG}"
+    echo "building nftables ${TAG} source package"
     
-    mkdir -p "${PACKAGE_DIR}/nftables"
-    cp -r py/* "${PACKAGE_DIR}/nftables"
-    tar -czf "${DIST_DIR}/${PACKAGE_NAME}" -C "${PACKAGE_DIR}" nftables
-    rm -rf "${PACKAGE_DIR}"
+    cd py
+    python -m build
+    mv ./dist "${DIST_DIR}/nftables-${TAG}"
+    cd -
 }
 
 build_packages() {
@@ -73,6 +94,8 @@ cleanup() {
 
 main() {
     validate_params "$@"
+    validate_python
+    print_python_version
     local WORKING_DIR
     WORKING_DIR="$(realpath "$1")"
     prepare_directories "${WORKING_DIR}"
